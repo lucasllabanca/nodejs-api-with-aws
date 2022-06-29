4ef1eb5073672710b28c66e72c091e33ee56c088
lucasllabanca <lucasllabanca@hotmail.com>
2022-06-29 14:06:42 -0300

    Initial commit after cdk init

---
+++ README.md
@@ -0,0 +1,12 @@
+# serverless-nodejs-api-with-aws
+
+NodeJS REST API with AWS CloudFormation using CDK, API Gateway, Lambda Functions, DynamoDB, Cloud Watch, SQS and Auto Scaling
+
+## Useful commands
+
+* `npm run build`   compile typescript to js
+* `npm run watch`   watch for changes and compile
+* `npm run test`    perform the jest unit tests
+* `cdk deploy`      deploy this stack to your default AWS account/region
+* `cdk diff`        compare deployed stack with current state
+* `cdk synth`       emits the synthesized CloudFormation template
---
+++ test/serverless-nodejs-api-with-aws.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ServerlessNodejsApiWithAws from '../lib/serverless-nodejs-api-with-aws-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/serverless-nodejs-api-with-aws-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ServerlessNodejsApiWithAws.ServerlessNodejsApiWithAwsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
